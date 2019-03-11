cc06529b613c09a040ed5d939e8c6ae79cf3e6d1
Adrien <napalm_ground@hotmail.com>
2019-03-11 14:36:26 +0100

    Classe Piece

---
+++ src/main/java/fr/rphstudio/chess/game/Piece.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.rphstudio.chess.game;
+
+import fr.rphstudio.chess.interf.IChess.ChessColor;
+import fr.rphstudio.chess.interf.IChess.ChessType;
+
+/**
+ *
+ * @author charneauadrien
+ */
+public class Piece
+{
+    private ChessColor color;
+    private ChessType type;
+    
+    public ChessColor getChessColor ()
+    {
+        return this.color;
+    }
+    
+    public ChessType getChessType ()
+    {
+        return this.type;
+    }
+}
